cfee532a2dbaa06435fbae91f2cf2d3ce62fb26c
Gλmix <94485066+Gapoly@users.noreply.github.com>
2024-12-18 17:09:56 +0100

    Update general.py

--- functions/general.py
+++ functions/general.py
@@ -2,14 +2,120 @@
 
 import os
 
-# 
+# 1. Web
+
+def web_install ():
+    print ("Installation d'Apache 2")
+    os.system("sudo apt-get install apache2 -y > /dev/null 2>&1")
+
+    php_choice=input("Voulez-vous installer PHP et ses dépendances ? [y/n] : ")
+
+    while True:
+        if php_choice == "y" or "Y":
+            print("Installation de PHP et de ses dépendances")
+            os.system("sudo apt-get install php curl php-curl -y > /dev/null 2>&1")
+            break
+        elif php_choice == "n" or "N":
+            print("Installation de PHP annulée")
+            break
+        else:
+            php_choice=input("Choix incorrect. Veuillez entrer 'y' ou 'n' ")
+
+    while True:
+        web_port=int(input("""Quel port voulez-vous ouvrir?
+        
+1. 80 - HTTP
+2. 443 - HTTPS
+3. 80 & 443
+4. Autre  
+        """))
+
+        if web_port == 1:
+            print("Ouverture du port 80")
+            os.system("sudo ufw allow 80 > /dev/null 2>&1")
+            break
+        elif web_port == 2:
+            print("Ouverture du port 443")
+            os.system("sudo ufw allow 443 > /dev/null 2>&1")
+            break
+        elif web_port == 3:
+            web_custom_port=int(input("Choisissez votre port : "))
+            print(f"Ouverture du port {web_custom_port}")
+            os.system(f"sudo ufw allow {web_custom_port}")
+            break
+        else:
+            print("Valeur incorrect")
+            continue
+
+def docker_install ():
+
+    from install import host
+
+    if host == "ubuntu":
+        os.system("""
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+""")
+        os.system("""
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+""")
+        os.system("sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin")
+        
+
+docker_install () {
+
+  host_executer=$(python3 ./conf/detection.py)
+
+  if [[ "$host_executer" == "ubuntu" ]]; then
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  else [[ "$host_executer" == "debian" ]];
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+}
+
 
 def openssh_install ():
 
     print("Installation de OpenSSH Server")
     os.system("sudo apt-get install openssh-server -y > /dev/null 2>&1")
     
-    ssh_port=int(input("Voulez vous utiliser le port par défaut? [y/n] "))
+    ssh_port=input("Voulez vous utiliser le port par défaut? [y/n] ")
 
     while True:
         if ssh_port == "y" or "Y":
